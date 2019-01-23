26355e0a01ae4b3ff5aecc79280f26224cbc5bbd
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 12:13:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,14 +8,9 @@ function teenager(age) {
   if (age < 13 || age > 19) {
     return "You are not a teenager"
   }
-  elsif(age >= 13 && age <= 19){
-  {""
-}
-
-function teenager(age) {
-  if (age >= 13 && age <= 19){
+  else if(age >= 13 && age <= 19){
     return "You are a teenager!"
-  }
+}
 }
 
 function ageChecker(age) {
