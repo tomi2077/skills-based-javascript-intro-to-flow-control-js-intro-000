707787dd9c8136d767dd9fa32e5dbe663b268722
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 11:52:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,11 +22,33 @@ function ageChecker(age) {
   }
 }
 
-function ternaryTeenager(age) {
+function ageChecker(age) {
+  if (age >= 20){
+      return "You are a grownup"
+  }
+}
 
+function ternaryTeenager(age) {
+  if (age >= 13 && age <= 19){
+      return "You are a teenager"
+  }
+}
+
+function ternaryTeenager(age) {
+  if (age < 13 || age > 19){
+      return "You are not a teenager"
+  }
+}
+
+
+function switchAge(age) {
+  if (age >= 13 && age <= 19){
+      return "You are a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if (age < 13 || age > 19){
+      return "You have an age"
+  }
 }
-
